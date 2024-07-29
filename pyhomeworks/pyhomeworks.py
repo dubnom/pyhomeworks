8b62c66259e619117e3380e6f5c14040a5fd3788
Erik <erik@montnemery.com>
2024-07-29 19:40:28 +0200

    Improve type annotations and run mypy in strict mode

--- pyhomeworks/pyhomeworks.py
+++ pyhomeworks/pyhomeworks.py
@@ -9,38 +9,40 @@ The Series4/8 is connected to an RS232 port to an Ethernet adaptor (NPort).
 Michael Dubno - 2018 - New York
 """
 
+from collections.abc import Callable
 import logging
 import select
 import socket
 import time
 from threading import Thread
+from typing import Any
 
 _LOGGER = logging.getLogger(__name__)
 
 POLLING_FREQ = 1.0
 
 
-def _p_address(arg):
+def _p_address(arg: str) -> str:
     return arg
 
 
-def _p_button(arg):
+def _p_button(arg: str) -> int:
     return int(arg)
 
 
-def _p_enabled(arg):
+def _p_enabled(arg: str) -> bool:
     return arg == "enabled"
 
 
-def _p_level(arg):
+def _p_level(arg: str) -> int:
     return int(arg)
 
 
-def _p_ledstate(arg):
+def _p_ledstate(arg: str) -> list[int]:
     return [int(num) for num in arg]
 
 
-def _norm(x):
+def _norm(x: str) -> tuple[str, Callable[[str], str], Callable[[str], int]]:
     return (x, _p_address, _p_button)
 
 
@@ -53,7 +55,7 @@ HW_KEYPAD_ENABLE_CHANGED = "keypad_enable_changed"
 HW_KEYPAD_LED_CHANGED = "keypad_led_changed"
 HW_LIGHT_CHANGED = "light_changed"
 
-ACTIONS = {
+ACTIONS: dict[str, tuple[str, Callable[[str], str], Callable[[str], Any]]] = {
     "KBP": _norm(HW_BUTTON_PRESSED),
     "KBR": _norm(HW_BUTTON_RELEASED),
     "KBH": _norm(HW_BUTTON_HOLD),
@@ -75,13 +77,15 @@ ACTIONS = {
 class Homeworks(Thread):
     """Interface with a Lutron Homeworks 4/8 Series system."""
 
-    def __init__(self, host, port, callback):
+    def __init__(
+        self, host: str, port: int, callback: Callable[[Any, Any], None]
+    ) -> None:
         """Connect to controller using host, port."""
         Thread.__init__(self)
         self._host = host
         self._port = port
         self._callback = callback
-        self._socket = None
+        self._socket: socket.socket | None = None
 
         self._running = False
         self._connect()
@@ -89,7 +93,7 @@ class Homeworks(Thread):
             raise ConnectionError(f"Couldn't connect to '{host}:{port}'")
         self.start()
 
-    def _connect(self):
+    def _connect(self) -> None:
         try:
             self._socket = socket.create_connection((self._host, self._port))
             # Setup interface and subscribe to events
@@ -102,24 +106,26 @@ class Homeworks(Thread):
         except (BlockingIOError, ConnectionError, TimeoutError):
             pass
 
-    def _send(self, command):
+    def _send(self, command: str) -> bool:
         _LOGGER.debug("send: %s", command)
         try:
-            self._socket.send((command + "\r").encode("utf8"))
+            self._socket.send((command + "\r").encode("utf8"))  # type: ignore[union-attr]
             return True
         except (ConnectionError, AttributeError):
             self._socket = None
             return False
 
-    def fade_dim(self, intensity, fade_time, delay_time, addr):
+    def fade_dim(
+        self, intensity: float, fade_time: float, delay_time: float, addr: str
+    ) -> None:
         """Change the brightness of a light."""
         self._send(f"FADEDIM, {intensity}, {fade_time}, {delay_time}, {addr}")
 
-    def request_dimmer_level(self, addr):
+    def request_dimmer_level(self, addr: str) -> None:
         """Request the controller to return brightness."""
         self._send(f"RDL, {addr}")
 
-    def run(self):
+    def run(self) -> None:
         """Read and dispatch messages from the controller."""
         self._running = True
         data = ""
@@ -144,7 +150,7 @@ class Homeworks(Thread):
                 except UnicodeDecodeError:
                     data = ""
 
-    def _process_received_data(self, data):
+    def _process_received_data(self, data: str) -> None:
         _LOGGER.debug("Raw: %s", data)
         try:
             raw_args = data.split(", ")
@@ -157,7 +163,7 @@ class Homeworks(Thread):
         except ValueError:
             _LOGGER.warning("Weird data: %s", data)
 
-    def close(self):
+    def close(self) -> None:
         """Close the connection to the controller."""
         self._running = False
         if self._socket:
