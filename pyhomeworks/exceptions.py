e2c872494e725b4b3cf6cc500fdddd87a360599d
Erik <erik@montnemery.com>
2024-07-30 18:05:52 +0200

    Don't connect or start thread from Homeworks constructor

--- pyhomeworks/exceptions.py
+++ pyhomeworks/exceptions.py
@@ -5,5 +5,9 @@ class HomeworksException(Exception):
     """Base class for exceptions."""
 
 
+class HomeworksConnectionFailed(HomeworksException):
+    """Connection failed."""
+
+
 class HomeworksConnectionLost(HomeworksException):
     """Connection lost."""
